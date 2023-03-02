a18d685fca4fdd6fb939235d473051718ab6fb8b
Alexey Sokolov <alexvyber@gmail.com>
2023-03-02 10:16:08 +0300

    finished 4-5 videos

--- working/day-2/src/scene.ts
+++ working/day-2/src/scene.ts
@@ -1,16 +1,5 @@
 import * as THREE from "three"
-// import {
-//   Scene,
-//   Color,
-//   BoxGeometry,
-//   MeshBasicMaterial,
-//   Mesh,
-//   PerspectiveCamera,
-//   WebGLRenderer,
-// } from "three"
-
-// console.log("🚀 ~ THREE:", THREE)
-// console.log(Object.keys(THREE).length)
+const { random } = Math
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl")
@@ -26,37 +15,116 @@ const sizes = {
 const scene = new THREE.Scene()
 scene.background = new THREE.Color("#f8f8f8")
 
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
 // Object
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 const cubeMaterial = new THREE.MeshBasicMaterial({
   color: "#ffe0d0",
 })
-
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+Object.assign(cubeMesh.position, {
+  x: 0,
+  y: -1,
+  z: 1,
+})
+Object.assign(cubeMesh.scale, {
+  x: 5,
+  y: 1,
+  z: 1,
+})
+
+// Math.random() > 0.5 && cubeMesh.position.normalize()
+
+// console.log("🚀 ~ cubeMesh.position:", cubeMesh.position)
+// console.log("🚀 ~ cubeMesh.position.length:", cubeMesh.position.length())
+
+function getRandom() {
+  return random() * (random() > 0.5 ? 1 : -1)
+}
+
+// cubeMesh.position.set(getRandom(), getRandom(), getRandom())
+
 scene.add(cubeMesh)
+
+/**
+ * Objects
+ */
+// const group = new THREE.Group()
+// group.scale.y = 2
+// group.rotation.y = 0.1
+// scene.add(group)
+
+// const cube1 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+// )
+// cube1.position.x = -1.5
+// group.add(cube1)
+
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+// )
+// cube2.position.x = 0
+// group.add(cube2)
+
+// const cube3 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+// )
+// cube3.position.x = 1.5
+// group.add(cube3)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas,
 })
 
-let p = 0
-renderer.setSize(sizes.width * 2, sizes.height * 2)
-setInterval(() => {
-  p += 1
-  console.log("🚀 ~ setInterval ~ p:", p)
-
-  // Camera
-  const camera = new THREE.PerspectiveCamera(p, sizes.width / sizes.height)
-  camera.position.z = 10
-  scene.add(camera)
-
-  renderer.render(scene, camera)
-}, 30)
-
-// const camera = new THREE.PerspectiveCamera(p, sizes.width / sizes.height)
-
+let p = 20
 // renderer.setSize(sizes.width * 2, sizes.height * 2)
-// camera.position.z = 10
-// scene.add(camera)
-// renderer.render(scene, camera)
+// setInterval(() => {
+//   p += 1
+//   console.log("🚀 ~ setInterval ~ p:", p)
+
+//   // Camera
+//   const camera = new THREE.PerspectiveCamera(p, sizes.width / sizes.height)
+//   camera.position.z = 10
+//   scene.add(camera)
+
+//   renderer.render(scene, camera)
+// }, 30)
+
+const camera = new THREE.PerspectiveCamera(p, sizes.width / sizes.height)
+
+// console.log(
+//   "🚀 ~ cubeMesh.position.distanceTo(camera.position):",
+//   cubeMesh.position.distanceTo(camera.position),
+// )
+
+renderer.setSize(sizes.width, sizes.height)
+camera.position.z = 20
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+
+cubeMesh.rotation.z = 1
+
+let i = 0
+// setInterval(
+//   () => (
+//     // cubeMesh.scale.set(getRandom(), getRandom(), getRandom()),
+//     // cubeMesh.position.set(getRandom(), getRandom(), getRandom()),
+//     (cubeMesh.rotation.y = i += 0.5), renderer.render(scene, camera)
+//   ),
+//   100,
+// )
+
+// cubeMesh.scale.set((getRandom() + 1) * 4, (getRandom() + 1) * 4, (getRandom() + 1) * 4)
+// cubeMesh.position.set(getRandom(), getRandom(), getRandom())
+
+renderer.render(scene, camera)
